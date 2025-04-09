9bb846821be0c5c97b733fe7708e8235da5652b6
Sangam Gupta <sangamg7318@gmail.com>
2025-04-09 14:27:09 +0530

    Initial deploy  commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,15 @@ function App() {
  };
 
   return (
+    <div  >
+     <div className='p-5 m-5'>
+          <h1 className='font-bold text-4xl'>User Management System</h1>
+      </div>
     <div className='flex justify-center '>
+     
       <AddUsers add={add}/>
         <ListUsers users={users} deleteUser={deleteUser}/>
+    </div>
     </div>
   )
 }
